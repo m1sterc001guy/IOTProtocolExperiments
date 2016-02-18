0a7bec13c94489a6e2c04ccd5f41402c6575a40e
Justin Moeller <b-jumoel@microsoft.com>
2016-02-18 01:36:27 +0000

    Implemented topics

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -cp ./jars/rabbitmq-java-client-bin-3.6.0/*:./target/GatewayProtocols-1.0-SNAPSHOT.jar networks.finalproject.GatewayProtocols
+java -cp ./jars/rabbitmq-java-client-bin-3.6.0/*:./target/GatewayProtocols-1.0-SNAPSHOT.jar networks.finalproject.GatewayProtocols "kern.warning" "A critical kernel error"
--- src/main/java/networks/finalproject/GatewayProtocols.java
+++ src/main/java/networks/finalproject/GatewayProtocols.java
@@ -9,26 +9,45 @@ import java.util.concurrent.TimeoutException;
 
 public class GatewayProtocols {
 
-  private final static String QUEUE_NAME = "hello";
+  private final static String EXCHANGE_NAME = "topic_logs";
 
   public static void main( String[] args ) throws IOException , TimeoutException {
-    System.out.println("Creating Factory");
     ConnectionFactory factory = new ConnectionFactory();
     factory.setUsername("test");
     factory.setPassword("test");
     factory.setHost("192.168.1.152");
-    System.out.println("Creating Connection");
     Connection connection = factory.newConnection();
-    System.out.println("Creating channel");
     Channel channel = connection.createChannel();
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Hello, World";
-    System.out.println("Publishing Message");
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-    System.out.println(" [x] Sent '" + message + "'");
+    channel.exchangeDeclare(EXCHANGE_NAME, "topic");
 
-    channel.close();
+    String routingKey = getRouting(args);
+    String message = getMessage(args);
+
+    channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+
     connection.close();
+  }
+
+  private static String getRouting(String[] strings) {
+    if (strings.length < 1) return "anonymous.info";
+    return strings[0];
+  }
+
+  private static String getMessage(String[] strings) {
+    if (strings.length < 2) return "Hello World!";
+    return joinStrings(strings, " ", 1);
+  }
+
+  private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+    int length = strings.length;
+    if (length == 0) return "";
+    if (length < startIndex) return "";
+    StringBuilder words = new StringBuilder(strings[startIndex]);
+    for (int i = startIndex + 1; i < length; i++) {
+      words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
   }
 }
