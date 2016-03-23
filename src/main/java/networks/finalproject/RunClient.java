cfb97c2de4ea87807ea8143b684fc64e08aa2355
Justin Moeller <b-jumoel@microsoft.com>
2016-03-23 20:36:19 +0000

    Simplified arguments

--- src/main/java/networks/finalproject/RunClient.java
+++ src/main/java/networks/finalproject/RunClient.java
@@ -1,41 +1,54 @@
 package networks.finalproject;
 
 import org.apache.log4j.Logger;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class RunClient {
 
   static Logger log = Logger.getLogger(RunClient.class.getName());
 
   public static void main(String[] args) {
-    if (args.length != 4) {
+    if (args.length != 2) {
       log.error("Insufficient number of arguments. Quitting...");
       System.exit(-1);
     }
-    String username = args[0];
-    String password = args[1];
-    String brokerHost = args[2];
-    boolean isSubscriber = Boolean.parseBoolean(args[3]);
 
-    String topic = "tigers";
+    String protocolType = args[0];
+    boolean isSubscriber = Boolean.parseBoolean(args[1]);
+    String brokerHost = "192.168.1.126";
+    IPubSub protocol = null;
+    String topic = "topic";
+    //String topic = "tigers";
 
-    // begin experiments
-    // username: test, password: test
-    //IPubSub protocol = new AMQPPubSub(username, password, brokerHost, "pubsub");
-    // username: doesn't matter, just needs to be different from other client
-    //IPubSub protocol = new MQTTPubSub(username, brokerHost, 2);
-    // TODO: Is there a way to send a message to a group?
-    // username: raspberrypi, password: raspberrypi
-    // username: tigers, password: raspberrypi2
-    //IPubSub protocol = new XMPPPubSub(username, password, brokerHost, isSubscriber);
-    IPubSub protocol = new COAPPubSub(brokerHost);
+    if (protocolType.equals("amqp")) {
+      protocol = new AMQPPubSub("test", "test", brokerHost, "pubsub");
+    } else if (protocolType.equals("mqtt")) {
+      String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+      protocol = new MQTTPubSub(timestamp, brokerHost, 2);
+    } else if (protocolType.equals("xmpp")) {
+      log.debug("TODO: DEBUG XMPP");
+    } else if (protocolType.equals("coap")) {
+      // TODO: change this back to broker host
+      protocol = new COAPPubSub("192.168.1.105");
+    } else {
+      log.debug("Unknown Protocol. Quitting...");
+      System.exit(-1);
+    }
+    
     protocol.connectToBroker();
 
     if (isSubscriber) {
       protocol.subscribe(topic);
     } else {
-      protocol.publish("Tigers are cool!", topic);
+      protocol.publish("MY MESSAGE", topic);
       protocol.close();
     }
+    
+    // TODO: Is there a way to send a message to a group?
+    // username: raspberrypi, password: raspberrypi
+    // username: tigers, password: raspberrypi2
+    //IPubSub protocol = new XMPPPubSub(username, password, brokerHost, isSubscriber);
   }
 
 }
